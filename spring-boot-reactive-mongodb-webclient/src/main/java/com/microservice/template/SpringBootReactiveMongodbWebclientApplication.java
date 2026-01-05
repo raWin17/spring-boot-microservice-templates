e1cf8ce91e522f1024f73cfd7ff7dcb3c8748346
Ravin <ravin17794@yahoo.com>
2026-01-05 21:09:11 +0000

    Initial project files

---
+++ spring-boot-reactive-mongodb-webclient/src/main/java/com/microservice/template/SpringBootReactiveMongodbWebclientApplication.java
@@ -0,0 +1,12 @@
+package com.microservice.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveMongodbWebclientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootReactiveMongodbWebclientApplication.class, args);
+  }
+}
---
+++ spring-boot-reactive-mongodb-webclient/src/test/java/com/microservice/template/SpringBootReactiveMongodbWebclientApplicationTests.java
@@ -0,0 +1,11 @@
+package com.microservice.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactiveMongodbWebclientApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
